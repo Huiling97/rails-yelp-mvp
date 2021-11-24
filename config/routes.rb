c806645aa56ae41284aeab14cf26c74ffb24aa22
Huiling Loh <huiling_loh_97@hotmail.com>
2021-11-24 12:40:34 +0800

    added created new restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "restaurants", to: "restaurants#index"
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
   get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
 end
