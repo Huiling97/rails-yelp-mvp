3f892855c3f986c4e0cb005154e80a3bcdd4536d
Huiling Loh <huiling_loh_97@hotmail.com>
2021-11-24 13:24:24 +0800

    new and create reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get "restaurants", to: "restaurants#index"
-  get 'restaurants/new', to: 'restaurants#new'
-  post 'restaurants', to: 'restaurants#create'
-  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
