152e52d6daaa845304a8f13e10268c50fd7a6887
Huiling Loh <huiling_loh_97@hotmail.com>
2021-11-24 12:06:34 +0800

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating 5 restaurants"
+
+5.times do |i|
+  Restaurant.create!({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  })
+end
+
+puts "5 restaurants created!"
